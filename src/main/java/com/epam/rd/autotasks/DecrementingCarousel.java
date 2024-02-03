2633287f77641c3e54025206b961ac12bab91185
Diana Solohub <sologubddd111@gmail.com>
2024-02-03 13:17:48 +0200

    change

--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,36 +1,15 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
-    protected final int[] array;
-    protected int index;
-    protected boolean running;
     public DecrementingCarousel(int capacity) {
-        this.array = new int[capacity];
-        for (int i = 0; i < capacity; i++){
-            array[i] = 0;
-        }
-        this.index = 0;
-        this.running = false;
+
     }
 
     public boolean addElement(int element){
-        if (element <= 0 || index == array.length || running){
-            return false;
-        }else {
-            array[index] = element;
-            index++;
-            return true;
-        }
-    }
-    public CarouselRun run(){
-        if (running){
-            return null;
-        }
-        running = true;
-        return new CarouselRun(array, index, getClassName());
+        throw new UnsupportedOperationException();
     }
 
-    protected Object getClassName(){
-        return getClass().getName();
+    public CarouselRun run(){
+       throw new UnsupportedOperationException();
     }
 }
--- src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarouselWithLimitedRun.java
@@ -1,22 +1,7 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarouselWithLimitedRun extends DecrementingCarousel{
-    private final int actionLimit;
     public DecrementingCarouselWithLimitedRun(final int capacity, final int actionLimit) {
         super(capacity);
-        this.actionLimit = actionLimit;
-    }
-    @Override
-    public boolean addElement(int element) {
-        return super.addElement(element);
-    }
-
-    @Override
-    public CarouselRun run() {
-        if (running){
-            return null;
-        }
-        running = true;
-        return new CarouselRun(array, index, getClassName(), actionLimit);
     }
 }
