5f3213141b3c2dc0d514a5902d4e6b278bf66f4a
dianaSolohub <sologubddd111@gmail.com>
2024-07-03 22:18:18 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/DecrementingCarousel.java
@@ -1,15 +1,36 @@
 package com.epam.rd.autotasks;
 
 public class DecrementingCarousel {
+    protected final int[] array;
+    protected int index;
+    protected boolean running;
     public DecrementingCarousel(int capacity) {
-
+        this.array = new int[capacity];
+        for (int i = 0; i < capacity; i++){
+            array[i] = 0;
+        }
+        this.index = 0;
+        this.running = false;
     }
 
     public boolean addElement(int element){
-        throw new UnsupportedOperationException();
+        if (element <= 0 || index == array.length || running){
+            return false;
+        }else {
+            array[index] = element;
+            index++;
+            return true;
+        }
+    }
+    public CarouselRun run(){
+        if (running){
+            return null;
+        }
+        running = true;
+        return new CarouselRun(array, index, getClassName());
     }
 
-    public CarouselRun run(){
-       throw new UnsupportedOperationException();
+    protected Object getClassName(){
+        return getClass().getName();
     }
 }
